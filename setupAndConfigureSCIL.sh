2a4cfc18bb015603859d044755762f63fb6c0f3e
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2013-07-08 09:24:00 -0400

    Updated Freesurfer version and changed tabs to spaces.

--- setupAndConfigureSCIL.sh
+++ setupAndConfigureSCIL.sh
@@ -74,7 +74,7 @@ then
     mkdir temp
     cd temp
     wget https://dl.dropbox.com/u/53085014/utils/mrtrix-0.2.11_2013-03-13.tar.bz2
-	tar -xvjf mrtrix-0.2.11_2013-03-13.tar.bz2
+    tar -xvjf mrtrix-0.2.11_2013-03-13.tar.bz2
 
     # Build
     cd mrtrix-0.2.11/
@@ -88,66 +88,66 @@ fi
 # Brainvisa
 if [[ $3 == 1 ]]
 then
-	echo
-	echo "*************************"
+    echo
+    echo "*************************"
     echo "Downloading and installing the Brainvisa suite."
     echo "*************************"
     echo
 
-	# This download will be deleted only at the end of the script, to make sure the user does not need to re-download it if something fails.	
-	wget -O /tmp/brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2 -c ftp://ftp.cea.fr/pub/dsv/anatomist/binary/brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2
-	cd /tmp/
-	tar -xjf brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2
-	sudo mv brainvisa-4.3.0/ /usr/local/
+    # This download will be deleted only at the end of the script, to make sure the user does not need to re-download it if something fails.    
+    wget -O /tmp/brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2 -c ftp://ftp.cea.fr/pub/dsv/anatomist/binary/brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2
+    cd /tmp/
+    tar -xjf brainvisa-Mandriva-2008.0-x86_64-4.3.0-2012_09_03.tar.bz2
+    sudo mv brainvisa-4.3.0/ /usr/local/
 fi
 
 # Freesurfer
 if [[ $4 == 1 ]]
 then
-	echo
-	echo "*************************"
+    echo
+    echo "*************************"
     echo "Downloading and installing Freesurfer."
     echo "*************************"
     echo
 
-	wget -c -O /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v5.2.0.tar.gz ftp://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/5.2.0/freesurfer-Linux-centos6_x86_64-stable-pub-v5.2.0.tar.gz
-	cd /tmp/
-	tar -xzf freesurfer-Linux-centos6_x86_64-stable-pub-v5.2.0.tar.gz
-	sudo mv freesurfer/ /usr/local/
+    wget -c -O /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v5.3.0.tar.gz ftp://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/5.3.0/freesurfer-Linux-centos6_x86_64-stable-pub-v5.3.0.tar.gz
+    cd /tmp/
+    tar -xzf freesurfer-Linux-centos6_x86_64-stable-pub-v5.3.0.tar.gz
+    sudo mv freesurfer/ /usr/local/
 
-	# Get the licence
-	cd ~
-	mkdir temp
-	cd temp/
-	wget https://dl.dropbox.com/u/53085014/utils/fs_licence
-	mv fs_licence /usr/local/freesurfer/.license
+    # Get the licence
+    cd ~
+    mkdir temp
+    cd temp/
+    wget https://dl.dropbox.com/u/53085014/utils/fs_licence
+    mv fs_licence /usr/local/freesurfer/.license
     cd ..
-	rm -r -f temp/
+    rm -r -f temp/
 fi
 
 # FSL 5.0
 if [[ $5 == 1 ]]
 then
-	echo
-	echo "*************************"
+    echo
+    echo "*************************"
     echo "Downloading and installing FSL."
     echo "*************************"
     echo
 
-	sudo apt-get install -y fsl
-	sudo ln -s /usr/share/fsl/5.0/ /usr/local/fsl
+    sudo apt-get install -y fsl
+    sudo ln -s /usr/share/fsl/5.0/ /usr/local/fsl
 fi
 
 # Fibernavigator dependencies
 if [[ $6 == 1 ]]
 then
-	echo
-	echo "*************************"
+    echo
+    echo "*************************"
     echo "Installing the Fibernavigator's dependencies."
     echo "*************************"
     echo
-	sudo apt-get install -y libwxbase2.8-dev libwxbase2.8-0 wx2.8-headers libwxgtk2.8-0 libwxgtk2.8-dev
-	sudo apt-get install -y libglew-dev
+    sudo apt-get install -y libwxbase2.8-dev libwxbase2.8-0 wx2.8-headers libwxgtk2.8-0 libwxgtk2.8-dev
+    sudo apt-get install -y libglew-dev
 fi
 
 # NLMEANS for denoising
@@ -181,42 +181,42 @@ fi
 
 if [[ $3 == 1 ]]
 then
-	echo
-	echo "- To be able to use Brainvisa / Anatomist when starting a new console, make"
-	echo "  sure to add the following lines to your .bashrc"
-	echo "    ###### Brainvisa ######"
-	echo "    export BRAINVISA=/usr/local/brainvisa-4.3.0/"
-	printf '    export PATH=$PATH:$BRAINVISA/bin'
-	echo
+    echo
+    echo "- To be able to use Brainvisa / Anatomist when starting a new console, make"
+    echo "  sure to add the following lines to your .bashrc"
+    echo "    ###### Brainvisa ######"
+    echo "    export BRAINVISA=/usr/local/brainvisa-4.3.0/"
+    printf '    export PATH=$PATH:$BRAINVISA/bin'
+    echo
 fi
 
 if [[ $4 == 1 ]]
 then
-	echo
-	echo "- To be able to use Freesurfer when starting a new console, make"
-	echo "  sure to add the following lines to your .bashrc"
-	echo "    ###### Freesurfer ######"
-	echo "    export FREESURFER_HOME=/usr/local/freesurfer"
-	echo '	  source $FREESURFER_HOME/SetUpFreeSurfer.sh'
+    echo
+    echo "- To be able to use Freesurfer when starting a new console, make"
+    echo "  sure to add the following lines to your .bashrc"
+    echo "    ###### Freesurfer ######"
+    echo "    export FREESURFER_HOME=/usr/local/freesurfer"
+    echo '    source $FREESURFER_HOME/SetUpFreeSurfer.sh'
 fi
 
 if [[ $5 == 1 ]]
 then
-	echo
-	echo "- To be able to use FSL when starting a new console, make"
-	echo "  sure to add the following lines to your .bashrc"
-	echo "    ###### FSL ######"
-	echo "    export FSLDIR=/usr/local/fsl"
-	echo '    export PATH=$PATH:${FSLDIR}/bin'
-	echo '    . ${FSLDIR}/etc/fslconf/fsl.sh'
+    echo
+    echo "- To be able to use FSL when starting a new console, make"
+    echo "  sure to add the following lines to your .bashrc"
+    echo "    ###### FSL ######"
+    echo "    export FSLDIR=/usr/local/fsl"
+    echo '    export PATH=$PATH:${FSLDIR}/bin'
+    echo '    . ${FSLDIR}/etc/fslconf/fsl.sh'
 fi
 
 if [[ $6 == 1 ]]
 then
-	echo
-	echo "- The Fibernavigator's dependencies have been installed."
-	echo "  You will still need to checkout the code and compile it."
-	echo "  See https://github.com/scilus/fibernavigator"
+    echo
+    echo "- The Fibernavigator's dependencies have been installed."
+    echo "  You will still need to checkout the code and compile it."
+    echo "  See https://github.com/scilus/fibernavigator"
 fi
 
 if [[ $install_nlmeans == true ]]
